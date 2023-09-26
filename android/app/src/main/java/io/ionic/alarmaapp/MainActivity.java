ea869b2a2decbba8313f78d6b73b9269ac5ffb67
unknown <juan.sueldo1989@gmail.com>
2023-09-25 21:46:40 -0300

    Splash e icon

--- android/app/src/main/java/io/ionic/alarmaapp/MainActivity.java
+++ android/app/src/main/java/io/ionic/alarmaapp/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.ionic.alarmaapp;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,30 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.alarmaapp',
   appName: 'alarmaApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  "plugins": {
+    "SplashScreen": {
+    "launchShowDuration": 3000,
+    "launchAutoHide": true,
+    "launchFadeOutDuration": 3000,
+    "backgroundColor": "#BF4342",
+    "androidSplashResourceName": "splash",
+    "androidScaleType": "CENTER_CROP",
+    "showSpinner": true,
+    "androidSpinnerStyle": "large",
+    "iosSpinnerStyle": "small",
+    "spinnerColor": "#76d3bd",
+    "splashFullScreen": true,
+    "splashImmersive": true,
+    "layoutName": "launch_screen",
+    "useDialog": true
+    }
+  
   }
 };
 
